5d53a86916edb427eea632e99279f938b7f24d74
Casper Ficke <c.ficke@purmerend.nl>
2019-02-04 15:02:18 +0100

    add reducer

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,17 +1,9 @@
 import React, { Component } from 'react';
 import Contact from './Contact';
 
-import { Consumer } from '../context'
+import { Consumer } from '../../context'
 
 class Contacts extends Component {
-  deleteContact = (id) => {
-    const { contacts } = this.state;
-
-    const newContacts = contacts.filter(contact => contact.id !== id);
-    this.setState({
-      contacts: newContacts
-    });
-  };
 
   render() {
     return (
@@ -24,7 +16,6 @@ class Contacts extends Component {
               <Contact
                 key={contact.id}
                 contact={contact}
-                deleteClickHandler= {this.deleteContact.bind(this, contact.id)}
               />
               ))}
             </React.Fragment>
