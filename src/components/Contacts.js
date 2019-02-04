efc3ab06df8948b9f5c681f6264ef290e9e89eac
Casper Ficke <c.ficke@purmerend.nl>
2019-02-04 12:34:32 +0100

    add context API

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,43 +1,37 @@
 import React, { Component } from 'react';
 import Contact from './Contact';
 
-class Contacts extends Component {
-  state = {
-  	contacts: [
-			{
-				id: 1,
-				name: 'John Doe',
-        email: 'johndoe@gmail.com',
-        phone: '555-555-5555'
-			},
-			{
-				id: 2,
-				name: 'Karen Smith',
-        email: 'karen@gmail.com',
-        phone: '333-333-3333'
-			},
-			{
-				id: 3,
-				name: 'Henry Johnson',
-        email: 'henry@gmail.com',
-        phone: '111-111-11111'
-			}
-  	]
-  }
+import { Consumer } from '../context'
 
-  render() {
+class Contacts extends Component {
+  deleteContact = (id) => {
     const { contacts } = this.state;
 
+    const newContacts = contacts.filter(contact => contact.id !== id);
+    this.setState({
+      contacts: newContacts
+    });
+  };
+
+  render() {
     return (
-      <div>
-      	{contacts.map(contact => (
-      		<Contact
-            key={contact.id}
-            contact={contact}
-          />
-      	))}
-      </div>
-    )
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+          return (
+            <React.Fragment>
+              {contacts.map(contact => (
+              <Contact
+                key={contact.id}
+                contact={contact}
+                deleteClickHandler= {this.deleteContact.bind(this, contact.id)}
+              />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
+    );
   }
 }
 
