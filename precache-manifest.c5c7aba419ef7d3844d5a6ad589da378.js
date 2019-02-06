59512b5a062a926e5fe5cb5c48c54f32bfc3ebf4
Casper Ficke <c.ficke@purmerend.nl>
2019-02-06 13:36:43 +0100

    Updates

---
+++ precache-manifest.c5c7aba419ef7d3844d5a6ad589da378.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "645ae89ef80a04ac6ae5",
+    "url": "/contactmanager/static/js/main.645ae89e.chunk.js"
+  },
+  {
+    "revision": "4354dbe3713c85ab9b0d",
+    "url": "/contactmanager/static/js/1.4354dbe3.chunk.js"
+  },
+  {
+    "revision": "645ae89ef80a04ac6ae5",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "4354dbe3713c85ab9b0d",
+    "url": "/contactmanager/static/css/1.d0f19f99.chunk.css"
+  },
+  {
+    "revision": "e60983f600604841ee52c66e01679497",
+    "url": "/contactmanager/index.html"
+  }
+];
